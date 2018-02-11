09965849c0239d43fdb11b36f778831805ebc7c9
JennySG <yujia_wang@berkeley.edu>
2018-02-11 09:50:26 -0800

    initial parsing done

--- test_generation/paperParser.py
+++ test_generation/paperParser.py
@@ -23,7 +23,7 @@ class paperParser():
 
 	# eliminate all comments (block comment and line comment)
 	def eliminate_comment(self):
-		# block comment: /* */
+		### eliminate block comment: /* */
 		paperList = self.paperString.split("/*")
 		print(len(paperList))
 		for i in range(1,len(paperList)):
@@ -35,16 +35,36 @@ class paperParser():
 				print("Error: there is no */ after a /*")
 		#sth = "".join(paperList)
 		#print(sth)
-		self.paper_list = [temp.split("\n") for temp in paperList]
-		print(len(self.paper_list))
 
+		### separate by \n + remove duplicates
+		paper_list = [temp.split("\n") for temp in paperList]
+		self.paperSet = set()
+		for i in paper_list:
+			[self.paperSet.add(j.strip()) for j in i]
+		self.paperSet.remove("")
+		#print(len(self.paperSet))
 
+		### eliminate line comment: %
+		tempSet = set()
+		for paragraph in self.paperSet:
+			if (paragraph.startswith("%")):
+				tempSet.add(paragraph)
+		self.paperSet.difference_update(tempSet)
+		#print(len(self.paperSet))
 
 
 	def parse_paper(self, fileName):
 		self.open_file(fileName)
 		self.select_main_document()
 		self.eliminate_comment()
+
+		# parse all paragraph into sentences
+		finalSet = set()
+		for i in self.paperSet:
+			for j in i.split("."):
+				finalSet.add(j)
+		print(len(finalSet))
+		return finalSet
 
 		
 
