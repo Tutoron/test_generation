2c2a0f79d75a1575a7ea381c46a4c9daf244aa0a
Lysia Li <lysiali@calvisitor-10-105-148-106.calvisitor.1918.berkeley.edu>
2018-02-13 10:36:11 -0800

    Good demo example

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from test_generation.paperParser import paperParser
 from test_generation import util
 
 pp = paperParser()
-sentences = list(pp.parse_paper("data/example_paper.tex"))
+# sentences = list(pp.parse_paper("data/example_paper.tex"))
+sentences = list(pp.parse_paper("data/fully connect paper/paper.tex"))
 
 detector = util.SymbolDetector()
 
+i = 1
+
 for s in sentences:
-    if detector.detect_symbol(s, '\\mathtt'):
+    if detector.detect_symbol(s, '\mb{f}'):
+    # if detector.detect_symbol(s, '\\mathtt'):
+        print("Number " + str(i))
+        i += 1
         print(s)
+        print("\n")
